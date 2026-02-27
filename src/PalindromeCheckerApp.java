8316c1573830e2504a3352bd4e5ca6f7f8e54cca
sherishadevi <sherishadevi@gmail.com.com>
2026-02-27 09:59:21 +0530

    UC6:queue+stack based

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,11 +7,110 @@ class UseCase1PalindromeCheckerApp{
 
     }
 }
+class UseCase2PalindromeCheckerApp{
+    String text="madam";
+    void uc2check(){
+        char[] chararray =text.toCharArray();
+        boolean ch=false;
+        for(int i=0;i<=text.length()/2;i++){
+            for(int j=text.length()-1;j>=text.length()/2;j--){
+                if(chararray[i]==chararray[j]){
+                    ch=true;
+                }
+            }
+        }
+        System.out.println(ch);
+    }
+}
+class UseCase3PalindromeCheckerApp{
+    void uc3check(String word){
+        String rev="";
+        boolean c3=false;
+        for(int i=word.length()-1;i>=0;i--){
+            rev=rev+word.charAt(i);
+        }
+        if(word==rev){
+            c3=true;
+        }
+        System.out.println(c3);
+    }
+}
+class UseCase4PalindromeCheckerApp{
+    void uc4check(String wor){
+        char[] chars=wor.toCharArray();
+        int start=0;
+        int end=chars.length-1;
+        boolean ispalindrome=false;
+        while(start<end){
+            if(chars[start]==chars[end]){
+                ispalindrome=true;
+                start++;
+                end--;
+            }
+        }
+        System.out.println(ispalindrome);
+    }
+}
+class UseCase5PalindromeCheckerApp {
+
+    Stack<Character> stack = new Stack<>();
+
+    void uc5check(String word) {
+        char[] ch = word.toCharArray();
+        boolean ispalindrome = false;
+        for (char c : word.toCharArray()) {
+            stack.push(c);
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if (stack.pop() == ch[i]) {
+                ispalindrome = true;
+            }
+        }
+        System.out.println(ispalindrome);
+
+    }
+}
+class UseCase6PalindromeCheckerApp {
+
+    void uc6check(String input) {
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println(isPalindrome);
+
+    }
+}
+
 
 public class PalindromeCheckerApp {
     public static void main(String[]args){
         UseCase1PalindromeCheckerApp welcome=new UseCase1PalindromeCheckerApp();
         welcome.display_welcome();
+        UseCase2PalindromeCheckerApp uc2=new UseCase2PalindromeCheckerApp();
+        uc2.uc2check();
+        UseCase3PalindromeCheckerApp uc3=new UseCase3PalindromeCheckerApp();
+        String wrd="";
+        uc3.uc3check(wrd);
+        UseCase4PalindromeCheckerApp uc4=new UseCase4PalindromeCheckerApp();
+        uc4.uc4check(wrd);
+        UseCase5PalindromeCheckerApp uc5 = new UseCase5PalindromeCheckerApp();
+        uc5.uc5check(wrd);
+        UseCase6PalindromeCheckerApp uc6 = new UseCase6PalindromeCheckerApp();
+        uc6.uc6check(wrd);
 
     }
 }
