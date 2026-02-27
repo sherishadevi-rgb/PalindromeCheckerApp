defd0811f0f9196abceb89a1c15ff57cd70fb6a4
sherishadevi <sherishadevi@gmail.com.com>
2026-02-27 09:19:26 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+
+class UseCase1PalindromeCheckerApp{
+    void display_welcome(){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+
+    }
+}
+
+public class PalindromeCheckerApp {
+    public static void main(String[]args){
+        UseCase1PalindromeCheckerApp welcome=new UseCase1PalindromeCheckerApp();
+        welcome.display_welcome();
+
+    }
+}
